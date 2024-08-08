4aac82b9974d76b265411fe1938f2ebde1335eda
Eduardo Noel <enoel.corebsd@gmail.com>
2024-08-08 18:20:52 +0200

    Test context load

--- src/test/java/org/example/transactions/TransactionsApplicationTests.java
+++ src/test/java/org/example/transactions/TransactionsApplicationTests.java
@@ -3,14 +3,17 @@ package org.example.transactions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 @ActiveProfiles("test")
 @SpringBootTest
 class TransactionsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Test context loads");
 	}
 
 }
